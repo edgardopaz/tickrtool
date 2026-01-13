d8332eedc9ab572071285e306a87620a6b60af1b
edgardopaz <dedgardo381@gmail.com>
2026-01-12 23:07:10 -0500

    api

--- tickr/tickr.py
+++ tickr/tickr.py
@@ -1,7 +1,12 @@
-import typer, requests, json
+import typer, requests, json, os
 from typing_extensions import Annotated
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = typer.Typer(help="CLI to pull daily updates on any stock.")
+API_KEY = os.getenv("ALPHA_VANTAGE_API_KEY")
+
 
 @app.command()
 def info(name: Annotated[str, typer.Argument(help="Enter the ticker symbol.")], force: Annotated[bool, typer.Option(prompt="Have you entered a valid ticker symbol?", help="Force information retrieval without confirmation.")]):
@@ -11,7 +16,7 @@ def info(name: Annotated[str, typer.Argument(help="Enter the ticker symbol.")], 
     If --force is not used, will ask for confirmation.
     """
     if force:
-        api_url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={name}&apikey=F9283COO17IJCEHK"
+        api_url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={name}&apikey={API_KEY}"
         response = requests.get(api_url)
         data = response.json()
         formatted_json = json.dumps(data, indent=4)
@@ -25,7 +30,7 @@ def search(name: Annotated[str, typer.Argument(help="Enter the company name to f
     """
     SEARCH for ticker symbols by inputting the company name to retrieve the most relevant results.
     """
-    api_url = f"https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={name}&apikey=F9283COO17IJCEHK"
+    api_url = f"https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={name}&apikey={API_KEY}"
     response = requests.get(api_url)
     data = response.json()
     top3 = data["bestMatches"][:3]
